db50ad492f9d50ce61948a93aab6409d2fdbfb57
Alisher Ruziyev <eclair.desertire@gmail.com>
2022-07-19 16:27:23 +0600

    added location, first axe, animaiton

--- Assets/MainScript.cs
+++ Assets/MainScript.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class MainScript : MonoBehaviour
 {
+    public List<GameObject> trees;
+    public List<GameObject> axes;
+
+    public int score = 0;
+    public int axe = 1;
+
     // Start is called before the first frame update
     void Start()
     {
