4475d05339d537d3e8ec7b5dec762e3738a4d9fc
Alisher Ruziyev <eclair.desertire@gmail.com>
2022-07-19 16:30:42 +0600

    Init new scriptable object

---
+++ Assets/Scripts/AxeScriptableObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeScriptableObject : ScriptableObject
+{
+    public int capacity = 50;
+    public int price = 50;
+    public int attack = 10;
+}
